0c4974c0cb356a8328ae38f026840da19eff2e3e
William B. Norton <wbn@drpeering.net>
2019-01-31 11:46:31 -0800

    getting set up

--- checkNewCode.bash
+++ checkNewCode.bash
@@ -1,13 +1,23 @@
 #!/bin/bash
 #
-#
 #		checkNewCode - reload new web site from github
+#			make 1 second granular checks and replaces during dev
 
 FILE=drpeering.com/newCode.semaphore
 
+i=0;
+while :
+do
 if [ -f $FILE ]; then
    echo `date`" File $FILE exists."
 	rm -rf drpeering.com
 	git clone https://github.com/williambnorton/drpeering.com.git drpeering.com
    echo `date`" NEW DRPEERING.COM."
 fi
+sleep 1
+i=`expr $i + 1`
+if [ $i -gt 58 ]; then
+	#echo `date` DONE
+	exit
+fi
+done
