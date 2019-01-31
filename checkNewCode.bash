4ec9cfd1d313f3121ac948f9cfb4e2fced90a3eb
William B. Norton <wbn@drpeering.net>
2019-01-31 11:11:37 -0800

    getting set up

--- checkNewCode.bash
+++ checkNewCode.bash
@@ -3,8 +3,11 @@
 FILE=drpeering.com/newCode.semaphore
 
 if [ -f $FILE ]; then
-   echo "File $FILE exists."
+   echo `date`" File $FILE exists."
+	mv drpeering.com drpeering.com-
+	git clone https://github.com/williambnorton/drpeering.com.git drpeering.com
 	#git pull https://github.com/williambnorton/drpeering.com.git
-	git pull https://github.com/williambnorton/drpeering.com.git
+	#git pull https://github.com/williambnorton/drpeering.com.git
 	rm $FILE
+   echo `date`" NEW DRPEERING.COM."
 fi
