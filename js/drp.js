af7937d5e2d690dab7674054ce982ed031552230
William B. Norton <wbn@drpeering.net>
2019-02-01 18:32:59 -0800

    ”SelfDriving”

--- js/drp.js
+++ js/drp.js
@@ -1,7 +1,7 @@
 //
 //	DrPeering javascript drp.js
 //
-var version=" v0.42";	//continuous updates	
+var version=" v0.43";	//continuous updates	
 var currentPage="";
 let timerId = setInterval(() => check(), 6000);
 
